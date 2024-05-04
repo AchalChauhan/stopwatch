15713d9c3197ced8faf53888b9615388ca1a378b
Achal Chauhan <43654070+AchalChauhan@users.noreply.github.com>
2024-05-04 14:14:55 +0530

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
   const formatTime = () => {
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
-    return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    return `${minutes < 10 ? '' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
   };
   
   return (
