cd7816b17793ad0aebd068d9cbff2e9cb28e8f9d
Achal Chauhan <43654070+AchalChauhan@users.noreply.github.com>
2024-05-04 13:54:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [isRunning, setIsRunning] = useState(false);
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setTime(prevTime => prevTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(intervalId);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isRunning]);
+
+
+  const startStop = () => {
+    setIsRunning(prevState => !prevState);
+  };
+  const reset = () => {
+    setIsRunning(false);
+    setTime(0);
+  };
+
+  const formatTime = () => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  };
+  
+  return (
+    <>
+    <div className="stopwatch">
+      <h1>Stopwatch</h1>
+      <h2>Time: <span>{formatTime()}</span></h2>
+      <div className="controls">
+        <button onClick={startStop}>{isRunning ? 'Stop' : 'Start'}</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
